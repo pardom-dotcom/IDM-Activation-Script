7e7b118b9bd3f42ca5a1e43baaf92b43421c42e0
pardom-dotcom <ahmedmiad310@gmail.com>
2026-06-27 17:45:48 +0600

    Update IAS.cmd

--- IAS.cmd
+++ IAS.cmd
@@ -18,7 +18,7 @@
 
 
 ::  To activate, run the script with "/act" parameter or change 0 to 1 in below line
-set _activate=0
+set _activate=1
 
 ::  To Freeze the 30 days trial period, run the script with "/frz" parameter or change 0 to 1 in below line
 set _freeze=0
